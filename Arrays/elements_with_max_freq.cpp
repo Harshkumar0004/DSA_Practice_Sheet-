83c8d638d5a21e69886045b06401ff6dd80ec6e5
Harsh Kumar <166305172+Harshkumar0004@users.noreply.github.com>
2025-10-14 20:06:45 +0530

    Update elements_with_max_freq.cpp

--- Arrays/elements_with_max_freq.cpp
+++ Arrays/elements_with_max_freq.cpp
@@ -1,22 +1,34 @@
-class Solution {
-public:
-    int maxFrequencyElements(vector<int>& nums) {
-        // count the frequency of each element using a hash map
-        unordered_map<int, int> mp;
-        for (auto i : nums)
-            mp[i]++;
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
-        // find the maximum frequency among all elements
-        int maxi = 0;
-        for (auto i : mp)
-            maxi = max(maxi, i.second);
+int maxFrequencyElements(vector<int>& nums) {
+    // count the frequency of each element using a hash map
+    unordered_map<int, int> mp;
+    for (auto i : nums)
+        mp[i]++;
 
-        // count total occurrences of all elements that have this maximum frequency
-        int cnt = 0;
-        for (auto i : mp) {
-            if (i.second == maxi)
-                cnt += i.second;
-        }
-        return cnt;
+    // find the maximum frequency among all elements
+    int maxi = 0;
+    for (auto i : mp)
+        maxi = max(maxi, i.second);
+
+    // count total occurrences of all elements that have this maximum frequency
+    int cnt = 0;
+    for (auto i : mp) {
+        if (i.second == maxi)
+            cnt += i.second;
     }
-};
+    return cnt;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 2, 3, 3, 3, 4};
+
+    int result = maxFrequencyElements(nums);
+
+    cout << "Total occurrences of elements with maximum frequency: " << result << endl;
+
+    return 0;
+}
