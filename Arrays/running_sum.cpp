deb1135fe223cd79431b57fea3af39774da08eb3
Harsh Kumar <166305172+Harshkumar0004@users.noreply.github.com>
2025-10-14 20:04:07 +0530

    Update running_sum.cpp

--- Arrays/running_sum.cpp
+++ Arrays/running_sum.cpp
@@ -1,14 +1,31 @@
-class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
-        //store the first element to calculate consecutive sums
-        int nextsum=nums[0];
-        for(int i=1;i<nums.size();i++){
-            //add the next element to nextsum
-            nextsum+=nums[i];
-            //replace the current element with nextsum
-            nums[i]=nextsum;
-        }
-        return nums;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> runningSum(vector<int>& nums) {
+    // store the first element to calculate consecutive sums
+    int nextsum = nums[0];
+
+    for (int i = 1; i < nums.size(); i++) {
+        // add the next element to nextsum
+        nextsum += nums[i];
+        // replace the current element with nextsum
+        nums[i] = nextsum;
     }
-};
+
+    return nums;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+
+    vector<int> result = runningSum(nums);
+
+    cout << "Running sum: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
