58e61c92f4850aa8843696a07e555f522e05f5f2
Harsh Kumar <166305172+Harshkumar0004@users.noreply.github.com>
2025-10-14 20:04:29 +0530

    Update maximum_minimum_elements.cpp

--- Arrays/maximum_minimum_elements.cpp
+++ Arrays/maximum_minimum_elements.cpp
@@ -1,40 +1,53 @@
-class Solution {
-public:
-    int minimumDeletions(vector<int>& nums) {
-        // indices of minimum and maximum elements
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
-        int minindex = 0, maxindex = 0;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-        for (int i = 0; i < nums.size(); i++) {
-            // update minimum element and its index
-            if (nums[i] < mini) {
-                mini = nums[i];
-                minindex = i;
-            }
-            // update maximum element and its index
-            if (nums[i] > maxi) {
-                maxi = nums[i];
-                maxindex = i;
-            }
+int minimumDeletions(vector<int>& nums) {
+    // indices of minimum and maximum elements
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+    int minindex = 0, maxindex = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        // update minimum element and its index
+        if (nums[i] < mini) {
+            mini = nums[i];
+            minindex = i;
         }
-        // compute how many deletions are needed from front and back
-        int front_min = minindex + 1;                
-        int front_max = maxindex + 1;               
-        int back_min = nums.size() - minindex;     
-        int back_max = nums.size() - maxindex;       
-
-        // all 3 possible ways
-
-        //  both from the front
-        int front_deletions = max(front_min, front_max);
-
-        // both from the back
-        int back_deletions = max(back_min, back_max);
-
-        // one from front and one from back
-        int mix_deletions = min(front_min + back_max, front_max + back_min);
-
-        return min({front_deletions, back_deletions, mix_deletions});
+        // update maximum element and its index
+        if (nums[i] > maxi) {
+            maxi = nums[i];
+            maxindex = i;
+        }
     }
-};
+
+    // compute how many deletions are needed from front and back
+    int front_min = minindex + 1;                
+    int front_max = maxindex + 1;               
+    int back_min = nums.size() - minindex;     
+    int back_max = nums.size() - maxindex;       
+
+    // all 3 possible ways
+
+    // both from the front
+    int front_deletions = max(front_min, front_max);
+
+    // both from the back
+    int back_deletions = max(back_min, back_max);
+
+    // one from front and one from back
+    int mix_deletions = min(front_min + back_max, front_max + back_min);
+
+    return min({front_deletions, back_deletions, mix_deletions});
+}
+
+int main() {
+    vector<int> nums = {48,-49,-67,18,-59,-56,47,-26,-24,-73,-96,27,-2,-45};
+
+    int result = minimumDeletions(nums);
+
+    cout << "Minimum deletions required: " << result << endl;
+
+    return 0;
+}
