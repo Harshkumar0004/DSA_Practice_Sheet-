fcef2cc87f44b62e05f1d053b07409ef61ddb35c
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2025-10-25 23:52:18 +0530

    Create preorder.cpp
    feat : Implemented preorder traversal on binary tree

---
+++ Binary Tree/preorder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+void preorder(TreeNode* root, vector<int>& res) {
+    if (root == nullptr)
+        return;
+    res.emplace_back(root->val);
+    preorder(root->left, res);
+    preorder(root->right, res);
+}
+
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    preorder(root, res);
+    return res;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    vector<int> result = preorderTraversal(root);
+    for (int val : result)
+        cout << val << " ";
+    cout << endl;
+    return 0;
+}
