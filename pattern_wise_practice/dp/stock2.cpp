085fefd74b560c2ba7bc3b17752b4ffc82d57e1e
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-09-06 18:45:18 +0530

    DSA Sheet DP

--- pattern_wise_practice/dp/stock2.cpp
+++ pattern_wise_practice/dp/stock2.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxp(int i, int b, int n, vector<int> &p)
+int maxp(int i, int b, int n, vector<int> &p, vector<vector<int>> &dp)
 {
     if (i == n)
     {
         return 0;
     }
+    if (dp[i][b] != -1)
+        return dp[i][b];
     if (b == 1)
     {
-        return max(-p[i] + maxp(i + 1, 0, n, p),
-                   maxp(i + 1, 1, n, p));
+        return dp[i][b] = max(-p[i] + maxp(i + 1, 0, n, p, dp), maxp(i + 1, 1, n, p, dp));
     }
     else
     {
-        return max(p[i] + maxp(i + 1, 1, n, p),
-                   maxp(i + 1, 0, n, p));
+        return dp[i][b] = max(p[i] + maxp(i + 1, 1, n, p, dp), maxp(i + 1, 0, n, p, dp));
     }
 }
 
@@ -23,6 +23,7 @@ int main()
 {
     vector<int> p = {7, 1, 6, 3, 8, 9, 10};
     int n = p.size();
-    cout << maxp(0, 1, n, p);
+    vector<vector<int>>dp(n,vector<int>(2,-1));
+    cout << maxp(0, 1, n, p,dp);
     return 0;
 }
