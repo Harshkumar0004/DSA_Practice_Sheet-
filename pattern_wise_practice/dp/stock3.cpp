ea88c444af66b5f4815c9559b461dd955f6e31a6
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-09-06 18:50:01 +0530

    DSA Sheet DP

--- pattern_wise_practice/dp/stock3.cpp
+++ pattern_wise_practice/dp/stock3.cpp
@@ -1,21 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxp(int i, int b, int n, int t, vector<int>& p) {
+
+int maxp(int i, int b, int n, int t, vector<int>& p, vector<vector<vector<int>>>& dp) {
     if (i == n) return 0;
     if (t == 0) return 0;
     
-    if (b == 1) {
-        return max(-p[i] + maxp(i + 1, 0, n, t, p), maxp(i + 1, 1, n, t, p));
-    }
-    else {
-        return max(p[i] + maxp(i + 1, 1, n, t - 1, p), maxp(i + 1, 0, n, t, p));
+    if (dp[i][b][t] != -1) return dp[i][b][t];
+
+    if (b == 1) { 
+        return dp[i][b][t] = max(-p[i] + maxp(i + 1, 0, n, t, p, dp), 
+                                  maxp(i + 1, 1, n, t, p, dp));
+    } else { 
+        return dp[i][b][t] = max(p[i] + maxp(i + 1, 1, n, t - 1, p, dp), 
+                                  maxp(i + 1, 0, n, t, p, dp));
     }
 }
 
 int main() {
     vector<int> ar = {7, 1, 4, 9, 4, 6, 0, 3};
     int n = ar.size();
-    cout << maxp(0, 1, n, 2, ar); 
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+    cout << maxp(0, 1, n, 2, ar, dp) << endl; 
+    
     return 0;
 }
