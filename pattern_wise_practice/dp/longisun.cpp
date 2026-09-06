9af252feda9189d6cc94d1f197d93b5bc62b1598
Harsh Kumar <166305172+harsh-space@users.noreply.github.com>
2026-09-06 20:49:35 +0530

    DSA Sheet DP

--- pattern_wise_practice/dp/longisun.cpp
+++ pattern_wise_practice/dp/longisun.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lis(int i, int n, int prev_idx, vector<int>& ar) {
+int lisMemo(int i, int n, int prev_idx, vector<int>& ar, vector<vector<int>>& dp) {
     if (i == n) {
         return 0;
     }
-    int exclude = lis(i + 1, n, prev_idx, ar);
+
+    if (dp[i][prev_idx + 1] != -1) {
+        return dp[i][prev_idx + 1];
+    }
+
+    int exclude = lisMemo(i + 1, n, prev_idx, ar, dp);
+
     int include = 0;
     if (prev_idx == -1 || ar[i] > ar[prev_idx]) {
-        include = 1 + lis(i + 1, n, i, ar);
+        include = 1 + lisMemo(i + 1, n, i, ar, dp);
     }
-    return max(include, exclude);
+
+    return dp[i][prev_idx + 1] = max(include, exclude);
 }
 
 int main() {
     vector<int> ar = {10, 9, 2, 5, 3, 7, 101, 18};
     int n = ar.size();
-
-    cout << lis(0, n, -1, ar) << endl;
+    
+    vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+    
+    cout << lisMemo(0, n, -1, ar, dp) << endl;
     
     return 0;
 }
